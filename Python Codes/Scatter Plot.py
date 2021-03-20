2dc66225447258d1751c05b63482c58f37481953
Jack Burdick <71950566+Burdj3@users.noreply.github.com>
2021-03-20 14:19:10 -0500

    Update Scatter Plot.py

--- Python Codes/Scatter Plot.py
+++ Python Codes/Scatter Plot.py
@@ -12,11 +12,14 @@ import matplotlib.pyplot as plt
 
 file = pd.read_excel(r'C:\Users\Jack Owen\Documents\College\Research Project\Pro Bowl Recievers.xlsx')
 
+# the original intent was for this to print out the graph for both recievers and running backs
+# it was easier to read seperately though
 file2 = pd.read_excel(r'C:\Users\Jack Owen\Documents\College\Research Project\Pro Bowl Running Backs.xlsx')
 
 measure1=file.dropna(subset=['Wt'])
 
-
+# can change "file" to "file2" to swap faster
+# also "Wt" can be changed to whichever measurement you'd like to compare against # of pro bowls
 x = file.Wt.dropna(how='any')
 
 y = measure1.Pbowls  # need to drop any missing categories
